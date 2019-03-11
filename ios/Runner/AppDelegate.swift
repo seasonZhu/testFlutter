9a7022d37e0fa96c0759dbff034897b869837b79
zhujilong <zhujilong2007@sina.com>
2019-03-11 09:50:37 +0800

    使用Swift的AppDelegate作为主入口,尝试进行内部Swift化编程

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Runner
+//
+//  Created by season on 2019/3/11.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate {
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        return true
+    }
+}
