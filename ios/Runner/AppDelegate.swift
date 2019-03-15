5b6d76b115a1210a2001f66f115354b07aa6ce50
zhujilong <zhujilong2007@sina.com>
2019-03-15 15:50:03 +0800

    添加原生的注释

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,31 +11,34 @@ import Foundation
 @UIApplicationMain
 class AppDelegate: FlutterAppDelegate {
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let controller = window?.rootViewController as! FlutterViewController
+        
+        //  加个导航栏
         let navi = UINavigationController(rootViewController: controller)
         navi.navigationBar.isHidden = true
         window.rootViewController = navi
-        let batteryChannel = FlutterMethodChannel(name: "samples.flutter.io/battery",
-                                                  binaryMessenger: controller)
-        batteryChannel.setMethodCallHandler({
+        
+        //  电池信息
+        let batteryChannel = FlutterMethodChannel(name: "samples.flutter.io/battery", binaryMessenger: controller)
+        batteryChannel.setMethodCallHandler {
             [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
             guard call.method == "getBatteryLevel" else {
                 result(FlutterMethodNotImplemented)
                 return
             }
             self?.receiveBatteryLevel(result: result)
-        })
+        }
         
-        let pushChannel = FlutterMethodChannel(name: "samples.flutter.io/pushToASwiftViewController",
-                                                  binaryMessenger: controller)
-        pushChannel.setMethodCallHandler({
+        //  推到原生界面
+        let pushChannel = FlutterMethodChannel(name: "samples.flutter.io/pushToASwiftViewController", binaryMessenger: controller)
+        pushChannel.setMethodCallHandler {
             [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
             guard call.method == "swiftPush" else {
                 result(FlutterMethodNotImplemented)
                 return
             }
             self?.pushToASwiftViewController()
-        })
+        }
         
         GeneratedPluginRegistrant.register(with: self)
         return true
